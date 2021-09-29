41871e975989694b24a98d8e6bd0cf45cd9633c5
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-09-29 14:08:51 -0700

    added javascript buttons to change nametag color

--- app.js
+++ app.js
@@ -2,6 +2,11 @@
 const nameInput = document.getElementById('name');
 const button = document.getElementById('generate');
 const firstName = document.getElementById('first-name');
+const buttonGreen = document.getElementById('button-green');
+const buttonBlue = document.getElementById('button-blue');
+const buttonYellow = document.getElementById('button-yellow');
+const header = document.getElementById('header');
+const footer = document.getElementById('footer');
 // initialize global state
 
 // set event listeners 
@@ -14,4 +19,21 @@ const firstName = document.getElementById('first-name');
     let nameValue = nameInput.value;
     firstName.textContent = nameValue;
   } 
+  );
+
+  buttonGreen.addEventListener('click', ()=>{
+    header.style.backgroundColor = "green";
+    footer.style.backgroundColor = "green";
+  
+  }
   )
+
+  buttonBlue.addEventListener('click', ()=>{
+    header.style.backgroundColor = "blue";
+    footer.style.backgroundColor = "blue";
+  } )
+
+  buttonYellow.addEventListener('click', ()=>{
+    header.style.backgroundColor = "yellow";
+    footer.style.backgroundColor = "yellow";
+  })
